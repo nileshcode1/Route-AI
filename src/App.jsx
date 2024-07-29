3a3750900b3b489dd9fdf13538793c512ed5e0dd
nileshcode1 <itsofficialcode@gmail.com>
2024-07-29 23:20:09 +0530

    codebase

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+ 
+
+  return (
+    <di>
+      <h1>HAG</h1>
+      <Button> Fuck off</Button>
+    </di>
+  )
+}
+
+export default App
